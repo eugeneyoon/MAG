e13bd4a77b55b9995ef10493d4d247058b312084
Eugene Yoon <44536657+eugeneyoon@users.noreply.github.com>
2021-02-07 02:05:19 -0800

    obstacle spawner

--- Assets/ChallengeSpawner.cs
+++ Assets/ChallengeSpawner.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ChallengeSpawner : MonoBehaviour
 {
+    public GameObject[] obstaclePrefabs;
+    public float spawnBoundX;
+    public float spawnBoundY;
+    public float spawnBoundZ;
+
     // Start is called before the first frame update
     void Start()
     {
         // spawn a bunch of random challenges
+        // from unity tutorial
+        InvokeRepeating("SpawnChallenge", 2, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
         // spawn some depending on how far up the player is or something 
+    }
+
+    void SpawnChallenge()
+    {
+        int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnBoundX, spawnBoundX), spawnBoundY, spawnBoundZ);
+
+        // hmm mess with rotation? 
+        Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
     }
 }
